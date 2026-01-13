7fc03a6d77530c15bff83391e04984b449564db7
ozubrzycka <166413070+ozubrzycka@users.noreply.github.com>
2026-01-13 10:59:55 +0100

    Update wykresy_sigmaENU.py.

--- wykresy_sigmaENU.py
+++ wykresy_sigmaENU.py
@@ -2,44 +2,42 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-# --- wartości referencyjne ---
-E_ref_CEBR = -10027.114672854777
-E_ref_VILL = 25257.097054654892
-E_ref_TOR  = 67612.36157869967
+# Wartosci referencyjne współrzędnych stacji:
+E_ref_HOA = -11717.483475578178
+E_ref_TJU = -46126.442650128265
+E_ref_ONSA  = -58076.34517919862
 
-N_ref_CEBR = 2701.771231459912
-N_ref_VILL = 1645.08125298489
-N_ref_TOR = 5138.876225557352
+N_ref_HOA = 28170.439823906483
+N_ref_TJU = 28082.640680199976
+N_ref_ONSA = -35180.94558484411
 
-U_ref_CEBR = -62.09812030468038
-U_ref_VILL = -232.2427890090852
-U_ref_TOR = -528.4575335533145
+U_ref_HOA = -95.15530320150538
+U_ref_TJU = -391.8129662269639
+U_ref_ONSA = -536.3832816155409
 
 # Analizowane dni:
 days_info = [
-    {"doy": 284, "trim": {"CEBR":1, "VILL":1, "TOR":1}, "num_points": 2878},
-    {"doy": 285, "trim": {"CEBR":1, "VILL":1, "TOR":1}, "num_points": 2878},
-    {"doy": 286, "trim": {"CEBR":1, "VILL":1, "TOR":1}, "num_points": 2878},
+    {"doy": 284, "trim": {"CEBR":1, "VILL":1, "TOR":1}, "num_points": 2879},
+    {"doy": 285, "trim": {"CEBR":1, "VILL":1, "TOR":1}, "num_points": 2879},
+    {"doy": 286, "trim": {"CEBR":1, "VILL":1, "TOR":1}, "num_points": 2879},
 ]
-
 
 all_cebr = []
 all_vill = []
 all_tor = []
-
 
 for day in days_info:
     doy = day["doy"]
     trim = day["trim"]
     num_points = day["num_points"]
     
-    df_cebr = pd.read_csv(f"ENU_MADR_CEBR_{doy}.csv", header=None, names=["E","N","U"])
-    df_vill = pd.read_csv(f"ENU_MADR_VILL_{doy}.csv", header=None, names=["E","N","U"])
-    df_tor  = pd.read_csv(f"ENU_MADR_TOR_{doy}.csv",  header=None, names=["E","N","U"])
+    df_cebr = pd.read_csv(f"ENU_SPT_HOA_{doy}.csv", header=None, names=["E","N","U"])
+    df_vill = pd.read_csv(f"ENU_SPT_TJU_{doy}.csv", header=None, names=["E","N","U"])
+    df_tor  = pd.read_csv(f"ENU_SPT_ONSA_{doy}.csv",  header=None, names=["E","N","U"])
     
-    diff_cebr = abs(df_cebr["U"] - U_ref_CEBR)[:-trim["CEBR"]]
-    diff_vill = abs(df_vill["U"] - U_ref_VILL)[:-trim["VILL"]]
-    diff_tor  = abs(df_tor["U"] - U_ref_TOR)[:-trim["TOR"]]
+    diff_cebr = (df_cebr["N"] - N_ref_HOA)[:-trim["CEBR"]]
+    diff_vill = (df_vill["N"] - N_ref_TJU)[:-trim["VILL"]]
+    diff_tor  = (df_tor["N"] - N_ref_ONSA)[:-trim["TOR"]]
     
     all_cebr.append(diff_cebr)
     all_vill.append(diff_vill)
@@ -50,32 +48,33 @@ diff_cebr_all = pd.concat(all_cebr).reset_index(drop=True)
 diff_vill_all = pd.concat(all_vill).reset_index(drop=True)
 diff_tor_all  = pd.concat(all_tor).reset_index(drop=True)
 
-
 total_points = len(diff_cebr_all)
 time_hours = np.arange(total_points) * (30/3600)
 
-# --- wykres ---
+# Wykres:
 plt.figure(figsize=(14,6))
 
-plt.scatter(time_hours, diff_vill_all, s=4, color="red", label="VILL00ESP")
-plt.scatter(time_hours, diff_tor_all,  s=4, color="orange", label="TOR100ESP")
-plt.scatter(time_hours, diff_cebr_all, s=4, color="green", label="CEBR00ESP")
+plt.scatter(time_hours, diff_vill_all, s=4, color="red", label="TJU000SWE")
+plt.scatter(time_hours, diff_tor_all,  s=4, color="orange", label="ONSA00SWE")
+plt.scatter(time_hours, diff_cebr_all, s=4, color="green", label="HOA000SWE")
 
-plt.ylim(0, 1.6)
-plt.yticks(np.arange(0, 1.61, 0.1))
+plt.ylim(-2, 2)
+plt.yticks(np.arange(-2.0, 2.01, 0.5), fontsize=12)
+plt.xticks(np.arange(0, 75, 3), fontsize=12)
+plt.yticks(np.arange(-2.0, 2.01, 0.5), fontsize=12)
 
 
-# Pionowe linie rezgraniczające dni analizy:
+# Pionowe linie przerywane - podział na 3 analizowane dni:
 for x in [24, 48]:
     plt.axvline(x=x, color='blue', linestyle='--', alpha=0.7) 
     
-    
+
 plt.xticks(np.arange(0, 75, 3))
-plt.xlabel("Czas (h) — 3 dni (DOY 284, 285, 286)")
-plt.ylabel("sigma E [m]")
+plt.xlabel("Czas (h) — 3 dni (DOY 284, 285, 286)", fontsize=14)
+plt.ylabel("sigma N [m]", fontsize=14)
 #plt.title("Odchylenia składowej pionowej U (NEU) względem pozycji referencyjnej - DOY 284, 285, 286")
 
 plt.grid(True)
-plt.legend()
+plt.legend(fontsize=12)
 plt.tight_layout()
 plt.show()
